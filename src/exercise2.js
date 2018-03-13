0a365641307d250a9c8c3b4b05fad64ba327d738
Valentin Guibert <valentinguibert@hotmail.fr>
2018-03-13 10:38:14 +0100

    Exercice 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,5 +1,22 @@
 var mapCustomer = function(customer) {
-  // Your future job begins here ...
+  if (Object.keys(customer).length === 0){
+  var customer = {      
+      firstname: 'John',
+      lastname: 'Doe',
+      products: [],
+      moneySpent: 0
+    };
+  }
+  else{
+    var sum = 0;
+    for (var i = 0; i < customer.products.length; i++) {
+    customer.products[i].price += sum;
+    };
+    
+    customer.moneySpent = sum;
+    ;
+  }
+return customer;
 }
 
 module.exports = {
