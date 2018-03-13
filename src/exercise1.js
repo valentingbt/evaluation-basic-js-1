b8b0b611f4c5cd6cb8c5888b3cf3aaa537a4dc0e
Valentin Guibert <valentinguibert@hotmail.fr>
2018-03-13 10:00:55 +0100

    Exercice 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,5 +1,17 @@
 var removeNegativeValue = function(values) {
-  // Your future job begins here ...
+  var filteredValues = [];
+  var z = 0;
+  if (values == null) {
+    filteredValues = [];
+  } else {
+  for (var i = 0; i < values.length; i++) {
+      if (values[i]>=0) {
+        filteredValues[z]=values[i];
+        z=z+1;
+      }
+  }
+}
+  return filteredValues;
 }
 
 module.exports = {
